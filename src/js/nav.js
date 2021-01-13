55fb47b2906d4ec7af5e2b9a994f565424f9ffe0
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-01-14 01:49:49 +0200

    Fix: logo align when mobile meun open

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,17 +1,23 @@
 const hamburger = document.querySelector('.header__nav-hamburger');
 const navItems = document.querySelector('.header__nav-items');
 const headerNav = document.querySelector('.header__nav');
+const headerlogo = document.querySelector('.header__logo');
 
-const closeMobileNav = (e) => {
-  navItems.classList.toggle('open');
-  headerNav.classList.toggle('header__nav--expand');
-  headerNav.classList.toggle('header__nav--shrink');
+const mq = window.matchMedia('(max-width: 1024px)');
+
+const toggleMobileNav = (e) => {
+  if (mq.matches) {
+    navItems.classList.toggle('open');
+    headerNav.classList.toggle('header__nav--expand');
+    headerNav.classList.toggle('header__nav--shrink');
+    headerlogo.classList.toggle('header__logo--alignstart');
+  }
 };
 
-hamburger.addEventListener('click', closeMobileNav);
+hamburger.addEventListener('click', toggleMobileNav);
 
 headerNav.addEventListener('click', (e) => {
   if (e.target.nodeName.toLowerCase() === 'a') {
-    closeMobileNav();
+    toggleMobileNav();
   }
 });
